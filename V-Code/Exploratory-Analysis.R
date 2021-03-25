c43f8241231e009b58f49c1a038be5b29909bd52
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-03-25 15:53:55 -0400

    Loaded csv data and took a preliminary glance

---
+++ V-Code/Exploratory-Analysis.R
@@ -0,0 +1,10 @@
+library(readr)
+
+##Read in ID csv
+IDS_0406 <- read_csv("IDS/IDS-0406.csv")
+
+IDS_0407 <- read_csv("IDS/IDS-0407.csv")
+
+##Read in Firewall csv
+Firewall_04062012 <- read_csv("Firewall 4/Firewall-04062012.csv")
+
